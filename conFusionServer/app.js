6cef3bbf7241e12869e7d829cc28ff789e795f96
Nishanth Bhat <nishanthbhat07@gmail.com>
2020-06-06 22:58:52 +0530

    REST API Express and MDB connected part1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -4,12 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter=require('./routes/dishRouter');
 var leaderRouter=require('./routes/leaderRouter');
 var promoRouter=require('./routes/promoRouter');
+const mongoose=require('mongoose');
+const Dishes=require('./models/dishes');
 
+const url='mongodb://localhost:27017/conFusion';
+const connect=mongoose.connect(url);
+connect.then((db)=>{
+  console.log('Connected to server');
+},err=>{console.log(err);});
 var app = express();
 
 // view engine setup
