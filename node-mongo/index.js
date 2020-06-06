65c28274ec848b7574af0555bdcf71f1d7515b0a
Nishanth Bhat <nishanthbhat07@gmail.com>
2020-06-06 19:21:25 +0530

    Using Promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,31 +5,32 @@ const dboper=require('./operations');
 const url='mongodb://localhost:27017';
 const dbname='conFusion';
 
-MongoClient.connect(url,(err,client)=>{
-    assert.equal(err,null);
+MongoClient.connect(url).then((client)=>{
+    
     console.log('Connected corectly to server');
     const db=client.db(dbname);
-    dboper.insertDocument(db,{name:"Vadonut",description:"Test1"},'dishes',(result)=>{
+    dboper.insertDocument(db,{name:"Vadonut",description:"Test1"},'dishes')
+    .then((result)=>{
         console.log('Insert Document:\n',result.ops);
 
-        dboper.findDocuments(db,'dishes',(docs)=>{
+       return  dboper.findDocuments(db,'dishes')
+    })
+    .then((docs)=>{
             console.log('FOund documents:\n',docs);
-
-            dboper.updateDocument(db,{name:"Vadonut"},{description:"updated test"},'dishes',(result)=>{
-                console.log("Updated Document:\n",result.result);
-                dboper.findDocuments(db,'dishes',(docs)=>{
-                    console.log('FOund documents:\n',docs);
-                   db.dropCollection('dishes',(result)=>{
-                       console.log('Dropped Collection: ',result);
-                       client.close();
-                   });
-
-
-                });
-            });
-
-        });
         
+           return dboper.updateDocument(db,{name:"Vadonut"},{description:"updated test"},'dishes')
+    })
+    .then((result)=>{
+            console.log("Updated Document:\n",result.result);
+            return dboper.findDocuments(db,'dishes')
+     })
+      .then((docs)=>{
+            console.log('FOund documents:\n',docs);
+            return db.dropCollection('dishes')
+    })
+    .then((result)=>{
+            console.log('Dropped Collection: ',result);
+            client.close();
+    }).catch((err)=>console.log(err));
 
-    });
-});
+}).catch((err)=>console.log(err));
