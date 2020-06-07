cf830c7d5cc6827f91ceb5729dd3cb8f74031b4e
Nishanth Bhat <nishanthbhat07@gmail.com>
2020-06-07 19:03:12 +0530

    Cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -28,32 +28,49 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-7777-9999'));
 
 function auth(req,res,next){
-  console.log(req.headers);
-  var authHeader=req.headers.authorization;
-  if(!authHeader){
-    var err =new Error("You are not authenticated");
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
+  console.log(req.signedCookies);
+  if(!req.signedCookies.user){
+    
+      var authHeader=req.headers.authorization;
+      if(!authHeader){
+        var err =new Error("You are not authenticated");
+        res.setHeader('WWW-Authenticate','Basic');
+        err.status=401;
 
-    return next(err);
-  }
-  var auth=new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+        return next(err);
+      }
+      var auth=new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
 
-  var usename=auth[0];
-  var password=auth[1];
+      var username=auth[0];
+      var password=auth[1];
 
-  if(usename=='admin' && password=='admin'){
-    next();
+      if(username=='admin' && password=='admin'){
+        res.cookie('user','admin',{signed:true});
+
+        next();
+      }
+      else{
+        var err =new Error("Wrong credentials");
+        res.setHeader('WWW-Authenticate','Basic');
+        err.status=401;
+        return next(err);
+      }
   }
   else{
-    var err =new Error("Wrong credentials");
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
+    if(req.signedCookies.user==='admin'){
+      next();
+    }
+    else{
+      var err =new Error("Wrong credentials");
+        
+        err.status=401;
+        return next(err);
+    }
   }
+
 
 
 }
