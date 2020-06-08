b4937c9322d73181278241f0a6db0d2377ed5093
Nishanth Bhat <nishanthbhat07@gmail.com>
2020-06-09 00:20:51 +0530

    HTTPS

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -23,7 +23,14 @@ connect.then((db)=>{
   console.log('Connected to server');
 },err=>{console.log(err);});
 var app = express();
-
+app.all('*',(req,res,next)=>{
+  if(req.secure){
+    return next();
+  }
+  else{
+    res.redirect(307,'https://'+req.hostname+':'+app.get('secPort')+req.url);
+  }
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
